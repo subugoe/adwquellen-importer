b53e86abebcb1853385a913cdfc845d13a5eadbc
Dennis Neumann <neumann@sub.uni-goettingen.de>
2017-09-28 15:00:22 +0000

    Add FWB Excel converter

---
+++ src/main/java/sub/adw/FwbExcelParser.java
@@ -0,0 +1,79 @@
+package sub.adw;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class FwbExcelParser {
+
+	private String[] headers = { "A 0 sort", "B 1 sigle", "C 2 kraftliste", "D 3 kurztitel", "E 4 ort und zeit", "F 5",
+			"G 6 raum / ort", "H 7 raum (karte)", "I 8", "J 9 zeit", "K 10 zeit numerisch", "L 11", "M 12 pdf",
+			"N 13 epdf", "O 14 digitalisat online", "P 15 eonline", "Q 16 permalink", "R 17 biblio", "S 18 ppn",
+			"T 19 zitierweise", "U 20 textsorte", "V 21 name", "W 22 sinnwelt", "X 23 klassifikation",
+			"Y 24 kommunikationsintention" };
+
+	private final int SIGLE = 1;
+	private final int KRAFTLISTE = 2;
+	private final int DIGITALISAT_ONLINE = 14;
+	private final int PERMALINK = 16;
+	private final int BIBLIO = 17;
+	private final int PPN = 18;
+	private final int ZITIERWEISE = 19;
+	private final int NAME = 21;
+
+	public List<Map<String, String>> convertExcelToMap(File excelFile) throws IOException {
+		FileInputStream file = new FileInputStream(excelFile);
+		List<Map<String, String>> resultList = new ArrayList<>();
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+		XSSFSheet sheet = workbook.getSheetAt(0);
+
+		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+			Map<String, String> resultMap = new HashMap<>();
+
+			Row row = sheet.getRow(i);
+			String biblio = asString(row.getCell(BIBLIO));
+			resultMap.put("biblio", biblio);
+
+			String ppn = asString(row.getCell(PPN));
+			resultMap.put("ppn", ppn);
+
+			resultList.add(resultMap);
+		}
+		workbook.close();
+		return resultList;
+	}
+
+	private String asString(Cell cell) {
+		String result = "";
+		if (cell != null && !isEmpty(cell)) {
+			if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+				result += cell.getStringCellValue();
+			} else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+				result += new Double(cell.getNumericCellValue()).intValue();
+			} else {
+				throw new RuntimeException("Unknown cell type: " + cell.getCellType() + ".");
+			}
+		}
+		return result.replace("<", "&lt;").replace(">", "&gt;").trim();
+	}
+
+	private boolean isEmpty(Cell cell) {
+		if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+			return cell.getStringCellValue().isEmpty();
+		} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/sub/adw/FwbExcelParserTest.java
@@ -0,0 +1,33 @@
+package sub.adw;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FwbExcelParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws Exception {
+		FwbExcelParser excelParser = new FwbExcelParser();
+		List<Map<String, String>> results = excelParser.convertExcelToMap(new File("src/test/resources/sources.xlsx"));
+
+		assertEquals("Biblio1", results.get(0).get("biblio"));
+		assertEquals("145520943", results.get(0).get("ppn"));
+		assertEquals("022714251", results.get(1).get("ppn"));
+	}
+
+}
