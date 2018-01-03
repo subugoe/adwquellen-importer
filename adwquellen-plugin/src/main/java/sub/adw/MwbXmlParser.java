5afe8f8a0e206ae582f9ba9812f14019120b95d5
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-01-03 15:03:40 +0000

    Add more MWB fields

--- adwquellen-plugin/src/main/java/sub/adw/MwbXmlParser.java
+++ adwquellen-plugin/src/main/java/sub/adw/MwbXmlParser.java
@@ -6,10 +6,12 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jdom2.Content;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.Namespace;
+import org.jdom2.Text;
 import org.jdom2.input.SAXBuilder;
 
 import com.google.common.collect.ArrayListMultimap;
@@ -29,12 +31,46 @@ public class MwbXmlParser {
 		SAXBuilder jdomBuilder = new SAXBuilder();
 		Document jdomDocument = jdomBuilder.build(xmlFile);
 		Element root = jdomDocument.getRootElement();
-		for (Element el : root.getChildren()) {
+		for (Element catalogEntry : root.getChildren()) {
 			ListMultimap<String, String> resultMap = ArrayListMultimap.create();
-			resultMap.put(SIGLE, el.getChildText("sigle", NS));
-			resultMap.put(BIBLIO, el.getChildTextNormalize("bibl", NS));
+			resultMap.put(ORIGIN, "mwb");
+			resultMap.put(SIGLE, catalogEntry.getChildText("sigle", NS));
+			resultMap.put(BIBLIO, getAllTextNormalize(catalogEntry.getChild("bibl", NS)));
+			addPpns(catalogEntry, resultMap);
 			resultList.add(resultMap);
 		}
 		return resultList;
+	}
+
+	private String getAllTextNormalize(Element el) {
+		String text = "";
+		if (el != null) {
+			List<Content> allSubs = el.getContent();
+			for (int i = 0; i < allSubs.size(); i++) {
+				Content sub = allSubs.get(i);
+				if (sub instanceof Text) {
+					String potentialPreSpace = "";
+					String potentialPostSpace = "";
+					if (i == 0 && sub.getValue().endsWith(" ")) {
+						potentialPostSpace = " ";
+					} else if (i == allSubs.size() - 1 && sub.getValue().startsWith(" ")) {
+						potentialPreSpace = " ";
+					}
+					text += potentialPreSpace + ((Text) sub).getTextNormalize() + potentialPostSpace;
+				} else if (sub instanceof Element) {
+					text += getAllTextNormalize((Element)sub);
+				}
+			}
+		}
+		return text;
+	}
+
+	private void addPpns(Element catalogEntry, ListMultimap<String, String> resultMap) {
+		Element ppns = catalogEntry.getChild("ppns", NS);
+		if (ppns != null) {
+			for (Element ppn : ppns.getChildren("ppn", NS)) {
+				resultMap.put(PPN, ppn.getTextTrim());
+			}
+		}
 	}
 }
--- adwquellen-plugin/src/test/java/sub/adw/MwbXmlParserTest.java
+++ adwquellen-plugin/src/test/java/sub/adw/MwbXmlParserTest.java
@@ -21,9 +21,34 @@ public class MwbXmlParserTest {
 	}
 
 	@Test
+	public void shouldAddNoPpns() throws Exception {
+		String solrXml = convert("ppnsEmpty.xml");
+		System.out.println(solrXml);
+		assertXpathEvaluatesTo("", "//field[@name='ppn']", solrXml);
+	}
+
+	@Test
+	public void shouldAddTwoPpns() throws Exception {
+		String solrXml = convert("ppns.xml");
+		assertXpathEvaluatesTo("12345678", "//field[@name='ppn'][1]", solrXml);
+		assertXpathEvaluatesTo("123456789", "//field[@name='ppn'][2]", solrXml);
+	}
+
+	@Test
+	public void shouldIgnoreInnerElementAndLinebreakInBiblio() throws Exception {
+		String solrXml = convert("biblWithElementAndBreak.xml");
+		assertXpathEvaluatesTo("Test bibliography with an inner element and a line break.", "//field[@name='biblio']", solrXml);
+	}
+
+	@Test
+	public void shouldIgnoreInnerElementInBiblio() throws Exception {
+		String solrXml = convert("biblWithElement.xml");
+		assertXpathEvaluatesTo("Test bibliography with an inner element.", "//field[@name='biblio']", solrXml);
+	}
+
+	@Test
 	public void shouldAddBiblio() throws Exception {
 		String solrXml = convert("bibl.xml");
-		System.out.println(solrXml);
 		assertXpathEvaluatesTo("Test bibliography with a linebreak.", "//field[@name='biblio']", solrXml);
 	}
 
@@ -31,6 +56,7 @@ public class MwbXmlParserTest {
 	public void shouldAddSigle() throws Exception {
 		String solrXml = convert("sigle.xml");
 		assertXpathEvaluatesTo("MySigle", "//field[@name='sigle']", solrXml);
+		assertXpathEvaluatesTo("mwb", "//field[@name='origin']", solrXml);
 	}
 
 	private String convert(String fileName) throws Exception {
